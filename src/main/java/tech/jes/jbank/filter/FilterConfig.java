4143f46695b5b426a622da920c214c80300f4cdb
John Enderson <jsilva@softexpert.com>
2024-12-21 12:48:59 -0300

    Criando e configurando o Filter de IP

---
+++ src/main/java/tech/jes/jbank/filter/FilterConfig.java
@@ -0,0 +1,26 @@
+package tech.jes.jbank.filter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    private final IpFilter ipFilter;
+
+
+    public FilterConfig(IpFilter ipFilter) {
+        this.ipFilter = ipFilter;
+    }
+
+    @Bean
+    public FilterRegistrationBean<IpFilter> filterRegistrationBean() {
+        var registrationBean = new FilterRegistrationBean<IpFilter>();
+
+        registrationBean.setFilter(ipFilter);
+        registrationBean.setOrder(0);
+
+        return registrationBean;
+    }
+}
