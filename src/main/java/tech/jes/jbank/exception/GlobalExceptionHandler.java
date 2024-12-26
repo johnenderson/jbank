78a657fbbe070ea7dd9569a2d6362dd850bf160f
John Enderson <jsilva@softexpert.com>
2024-12-26 16:37:19 -0300

    Tratando o retorno da API após as validações do Hibernate Validator

--- src/main/java/tech/jes/jbank/exception/GlobalExceptionHandler.java
+++ src/main/java/tech/jes/jbank/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package tech.jes.jbank.exception;
 
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import tech.jes.jbank.exception.dto.InvalidParamDto;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -10,6 +12,23 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(JBankException.class)
     public ProblemDetail handleJBankException(JBankException e) {
         return e.toProblemDetail();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        var invalidParams = e.getFieldErrors()
+                .stream()
+                .map(fe -> new InvalidParamDto(fe.getField(), fe.getDefaultMessage()))
+                .toList();
+
+        var pd = ProblemDetail.forStatus(400);
+
+        pd.setTitle("Invalid request parameters");
+        pd.setDetail("There is invalid fields on the request");
+        pd.setProperty("invalid-params", invalidParams);
+
+        return pd;
     }
 
 }
---
+++ src/main/java/tech/jes/jbank/exception/dto/InvalidParamDto.java
@@ -0,0 +1,4 @@
+package tech.jes.jbank.exception.dto;
+
+public record InvalidParamDto(String field, String reason) {
+}
